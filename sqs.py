b3a9079f06442f96f9531f13780d9c6099b284a3
Maximiliano Basoalto <ztwblizzard@gmail.com>
2019-07-01 00:06:27 -0300

    Update

--- sqs.py
+++ sqs.py
@@ -1,30 +1,63 @@
 #!/usr/bin/python3
 import boto3
 import json
+from botocore.exceptions import ClientError
 
-def toQueue( obj, context ):
+# Push in batch 10 messages
+def toBatch( dic, context ):
     total = 0
     last = 0
-    params = {}
-    params[last] = {}
-    params[last]['Entries'] = []
-    params[last]['QueueUrl'] = obj['QueueUrl']
+    params = {
+        last: {
+            "Entries": [],
+            "QueueUrl": dic["QueueUrl"]
+        }
+    }
 
-    if len(obj['entries']):
-        for index, msg in enumerate(obj['entries']):
-            params[last]['Entries'].append({'Id': str(index), 'MessageBody': json.dumps(msg)})
+    if len(dic["Entries"]):
+        attributes = {}
+
+        if len(dic["Entries"]["MessageAttributes"]):
+            for index, key in enumerate(dic["Entries"]["MessageAttributes"]):
+                attributes[key] = {
+                    "StringValue": dic["Entries"]["MessageAttributes"][key],
+                    "DataType": "String"
+                }
+
+        for i, entry in enumerate(dic["Entries"]["MessageBody"]):
+            if len(attributes):
+                params[last]["Entries"].append({ "Id": str(i), "MessageBody": json.dumps(entry), "MessageAttributes": attributes })
+            else:
+                params[last]["Entries"].append({ "Id": str(i), "MessageBody": json.dumps(entry) })
             
-            if (index != 0 and (index % 9) == 0):
+            if (i != 0 and (i % 9) == 0):
                 last += 1
-                params[last] = {}
-                params[last]['Entries'] = []
-                params[last]['QueueUrl'] = obj['QueueUrl']
+                params[last] = {
+                    "Entries": [],
+                    "QueueUrl": dic["QueueUrl"]
+                }
 
         if len(params):
-            sqs = boto3.client('sqs')
+            sqs = boto3.client("sqs")
             
-            for i in params:
-                response = sqs.send_message_batch( QueueUrl=params[i]['QueueUrl'], Entries=params[i]['Entries'] )
-                if len(response) and response['Successful'] and response['ResponseMetadata'] and response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            for idx in params:
+                response = sqs.send_message_batch( QueueUrl=params[idx]["QueueUrl"], Entries=params[idx]["Entries"] )
+                if len(response) and response["Successful"] and response["ResponseMetadata"] and response["ResponseMetadata"]['HTTPStatusCode'] == 200:
                     total += 1
     return total
+
+# Single message push
+def toQueue( obj, context ): return False
+
+def getQueue( obj, context ):
+    try:
+        sqs = boto3.client('sqs')
+        queue = sqs.get_queue_url(
+            QueueName=obj['url'],
+            QueueOwnerAWSAccountId=obj['account']
+        )
+        return { 'Ok': True, 'Error': False }
+    except ClientError as e:
+        return { 'Ok': False, 'Error': True, 'Mensaje': e.response['Error']['Message'], 'Codigo': e.response['ResponseMetadata']['HTTPStatusCode'] }
+
+def createQueue( obj, context ): return False
